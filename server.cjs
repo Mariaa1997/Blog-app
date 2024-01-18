0b179bac346564baa01a6c6af5c53d97a4c20d91
Mariaa1997 <mariaaguilar_95@yahoo.com>
2024-01-17 19:23:28 -0500

    middleware

--- server.cjs
+++ server.cjs
@@ -24,6 +24,7 @@ const connectDB = async () => {
 //middlewares
 dotenv.config();
 app.use(express.json());
+app.use('/', express.static('dist'))
 app.use('/images', express.static(path.join(__dirname,"/images")))
 app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cookieParser())
